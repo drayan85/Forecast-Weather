a991dfbc6935774f5c63c7ed1716f3b4014fcbba
Ilanthirayan <theebankala@gmail.com>
2016-09-08 16:29:09 +0800

    Constants class is created to pre-define entities (Ex : default location)

---
+++ app/src/main/java/com/forecast/weather/Constants.java
@@ -0,0 +1,11 @@
+package com.forecast.weather;
+
+/**
+ * Created by Ilanthirayan on 7/9/16.
+ */
+public class Constants {
+
+    public static final String  DEFAULT_CITY = "Singapore";
+    public static final String  DEFAULT_LAT = "1.290270";
+    public static final String  DEFAULT_LON = "103.851959";
+}
