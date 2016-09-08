ac1463e7569a7985fd0a8b9135cb7c0dd59c0707
Ilanthirayan <theebankala@gmail.com>
2016-09-08 16:58:10 +0800

    Task out put enum is created

---
+++ app/src/main/java/com/forecast/weather/tasks/TaskOutput.java
@@ -0,0 +1,11 @@
+package com.forecast.weather.tasks;
+
+/**
+ * Created by Ilanthirayan on 7/9/16.
+ */
+public class TaskOutput {
+    // Indicates result of parsing server response
+    ParseResult parseResult;
+    // Indicates result of background task
+    TaskResult taskResult;
+}
